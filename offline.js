0c035a0f6fc38b98a98ce7bb0ba091dee7ca9731
MHryshunina <84012286+MHryshunina@users.noreply.github.com>
2022-06-24 22:12:23 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1656097843,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/road-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/car-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
